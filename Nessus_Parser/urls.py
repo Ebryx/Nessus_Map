385c58fccab1f18ae70ef34d2521a6f24a6e2cf2
d3vilbug <slaveofwahab1@gmail.com>
2019-09-12 16:10:33 +0500

    Nessus Map v2.0

--- Nessus_Parser/urls.py
+++ Nessus_Parser/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     url(r'services/$', views.do_port_filter, name='services'),
     url(r'parseOS/$', views.do_parse_os, name='parseos'),
     url(r'executive-report/$', views.generate_executive_report, name='executive-report'),
+    url(r'gen-html', views.generate_html_report, name="gen-html-report"),
+    url(r'gen-json', views.generate_json_file, name="gen-json"),
+    url(r'load-json', views.load_json_file, name="load-json")
 ]
 
